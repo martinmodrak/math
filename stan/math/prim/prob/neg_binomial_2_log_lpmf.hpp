0401ec870d35eb78ab3110c2388cb724d806bbbc
martin_cerny <cerny.m@gmail.com>
2020-01-06 15:40:54 +0100

    Some tests, some improvements

--- stan/math/prim/prob/neg_binomial_2_log_lpmf.hpp
+++ stan/math/prim/prob/neg_binomial_2_log_lpmf.hpp
@@ -14,6 +14,12 @@
 namespace stan {
 namespace math {
 
+namespace internal {
+  //TODO[martinmodrak] merge with neg_binomial_2_phi_cutoff once #1497 is 
+  //processed
+  constexpr double neg_binomial_2_log_phi_cutoff = 1e10;
+}
+
 // NegBinomial(n|eta, phi)  [phi > 0;  n >= 0]
 template <bool propto, typename T_n, typename T_log_location,
           typename T_precision>
@@ -54,59 +60,92 @@ return_type_t<T_log_location, T_precision> neg_binomial_2_log_lpmf(
   size_t len_ep = max_size(eta, phi);
   size_t len_np = max_size(n, phi);
 
-  VectorBuilder<true, T_partials_return, T_log_location> eta__(size(eta));
+  VectorBuilder<true, T_partials_return, T_log_location> eta_val(size(eta));
   for (size_t i = 0, max_size_seq_view = size(eta); i < max_size_seq_view;
        ++i) {
-    eta__[i] = value_of(eta_vec[i]);
+    eta_val[i] = value_of(eta_vec[i]);
   }
 
-  VectorBuilder<true, T_partials_return, T_precision> phi__(size(phi));
+  VectorBuilder<true, T_partials_return, T_precision> phi_val(size(phi));
   for (size_t i = 0, max_size_seq_view = size(phi); i < max_size_seq_view;
        ++i) {
-    phi__[i] = value_of(phi_vec[i]);
+    phi_val[i] = value_of(phi_vec[i]);
   }
 
   VectorBuilder<true, T_partials_return, T_precision> log_phi(size(phi));
   for (size_t i = 0, max_size_seq_view = size(phi); i < max_size_seq_view;
        ++i) {
-    log_phi[i] = log(phi__[i]);
+    log_phi[i] = log(phi_val[i]);
   }
 
   VectorBuilder<true, T_partials_return, T_log_location, T_precision>
       logsumexp_eta_logphi(len_ep);
   for (size_t i = 0; i < len_ep; ++i) {
-    logsumexp_eta_logphi[i] = log_sum_exp(eta__[i], log_phi[i]);
+    logsumexp_eta_logphi[i] = log_sum_exp(eta_val[i], log_phi[i]);
   }
 
   VectorBuilder<true, T_partials_return, T_n, T_precision> n_plus_phi(len_np);
   for (size_t i = 0; i < len_np; ++i) {
-    n_plus_phi[i] = n_vec[i] + phi__[i];
+    n_plus_phi[i] = n_vec[i] + phi_val[i];
   }
 
   for (size_t i = 0; i < max_size_seq_view; i++) {
-    if (include_summand<propto>::value) {
-      logp -= lgamma(n_vec[i] + 1.0);
-    }
-    if (include_summand<propto, T_precision>::value) {
-      logp += multiply_log(phi__[i], phi__[i]) - lgamma(phi__[i]);
-    }
-    if (include_summand<propto, T_log_location>::value) {
-      logp += n_vec[i] * eta__[i];
-    }
-    if (include_summand<propto, T_precision>::value) {
-      logp += lgamma(n_plus_phi[i]);
-    }
-    logp -= (n_plus_phi[i]) * logsumexp_eta_logphi[i];
+   if (phi_val[i] > internal::neg_binomial_2_log_phi_cutoff) {
+      // Phi is large, delegate to Poisson.
+      // Copying the code here as just calling
+      // poisson_lpmf does not preserve propto logic correctly.
+      // Note that Poisson can be seen as first term of Taylor series for
+      // phi -> Inf. Similarly, the derivative wrt mu and phi can be obtained
+      // via the Same Taylor expansions, in Mathematica using the code:
+      //
+      // nb2log[n_,eta_,phi_]:= LogGamma[n + phi] - LogGamma[n + 1] - 
+      //     LogGamma[phi ] + n * (eta - Log[Exp[eta] + phi]) + 
+      //     phi * (Log[phi] - Log[Exp[eta] + phi]);
+      // nb2logdeta[n_,eta_,phi_]= D[nb2log[n, eta, phi],eta];
+      // nb2logdphi[n_,eta_,phi_]= D[nb2log[n, eta, phi],phi];
+      // Series[nb2logdeta[n, eta, phi],{phi, Infinity, 0}]
+      // Series[nb2logdphi[n, eta, phi],{phi, Infinity, 2}]      
+      //
+      // The derivative wrt phi = 0 + O(1/phi^2),
+      // But the quadratic term is big enough to warrant inclusion here
+      // (can be around 1e-6 at cutoff).
+      if (include_summand<propto>::value) {
+        logp -= lgamma(n_vec[i] + 1.0);
+      }
+      double exp_eta = exp(eta_val[i]);
+      if (include_summand<propto, T_log_location>::value) {
+        logp += eta_val[i] * n_vec[i] - exp_eta;
+      }
 
-    if (!is_constant_all<T_log_location>::value) {
-      ops_partials.edge1_.partials_[i]
-          += n_vec[i] - n_plus_phi[i] / (phi__[i] / exp(eta__[i]) + 1.0);
-    }
-    if (!is_constant_all<T_precision>::value) {
-      ops_partials.edge2_.partials_[i]
-          += 1.0 - n_plus_phi[i] / (exp(eta__[i]) + phi__[i]) + log_phi[i]
-             - logsumexp_eta_logphi[i] - digamma(phi__[i])
-             + digamma(n_plus_phi[i]);
+      if (!is_constant_all<T_log_location>::value) {
+        ops_partials.edge1_.partials_[i] += n_vec[i] - exp_eta;
+      }
+      if (!is_constant_all<T_precision>::value) {
+        ops_partials.edge2_.partials_[i]
+            += (exp_eta * (-exp_eta + 2 * n_vec[i])
+                + n_vec[i] * (1 - n_vec[i]))
+               / (2 * square(phi_val[i]));
+      }
+    } else {
+      if (include_summand<propto, T_precision>::value) {
+        logp += binomial_coefficient_log(n_plus_phi[i] - 1, n_vec[i]);
+      }
+      if (include_summand<propto, T_log_location>::value) {
+        logp += n_vec[i] * eta_val[i];
+      }
+      logp += phi_val[i] * (log_phi[i] - logsumexp_eta_logphi[i])
+              - n_vec[i] * logsumexp_eta_logphi[i];
+
+      if (!is_constant_all<T_log_location>::value) {
+        ops_partials.edge1_.partials_[i]
+            += n_vec[i] - n_plus_phi[i] / (phi_val[i] / exp(eta_val[i]) + 1.0);
+      }
+      if (!is_constant_all<T_precision>::value) {
+        ops_partials.edge2_.partials_[i]
+            += 1.0 - n_plus_phi[i] / (exp(eta_val[i]) + phi_val[i]) + log_phi[i]
+              - logsumexp_eta_logphi[i] - digamma(phi_val[i])
+              + digamma(n_plus_phi[i]);
+      }
     }
   }
   return ops_partials.build(logp);
--- test/unit/math/prim/prob/neg_binomial_2_log_test.cpp
+++ test/unit/math/prim/prob/neg_binomial_2_log_test.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 #include <string>
 
-TEST(ProbDistributionsNegativeBinomial2Log, errorCheck) {
+TEST(ProbDistributionsNegBinomial2Log, errorCheck) {
   check_dist_throws_all_types(NegativeBinomial2LogTestRig());
 }
 
@@ -183,28 +183,57 @@ TEST(ProbDistributionsNegBinomial2Log, chiSquareGoodnessFitTest3) {
   EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
 }
 
-TEST(ProbNegBinomial2, log_matches_lpmf) {
-  double y = 0.8;
-  double mu = 1.1;
+TEST(ProbDistributionsNegBinomial2Log, log_matches_lpmf) {
+ double y = 0.8;
+  double eta = 1.1;
   double phi = 2.3;
 
-  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_lpmf(y, mu, phi)),
-                  (stan::math::neg_binomial_2_log(y, mu, phi)));
-  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_lpmf<true>(y, mu, phi)),
-                  (stan::math::neg_binomial_2_log<true>(y, mu, phi)));
-  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_lpmf<false>(y, mu, phi)),
-                  (stan::math::neg_binomial_2_log<false>(y, mu, phi)));
+  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_log_lpmf(y, eta, phi)),
+                  (stan::math::neg_binomial_2_log_log(y, eta, phi)));
+  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_log_lpmf<true>(y, eta, phi)),
+                  (stan::math::neg_binomial_2_log_log<true>(y, eta, phi)));
+  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_log_lpmf<false>(y, eta, phi)),
+                  (stan::math::neg_binomial_2_log_log<false>(y, eta, phi)));
   EXPECT_FLOAT_EQ(
-      (stan::math::neg_binomial_2_lpmf<true, double, double, double>(y, mu,
-                                                                     phi)),
-      (stan::math::neg_binomial_2_log<true, double, double, double>(y, mu,
-                                                                    phi)));
+      (stan::math::neg_binomial_2_log_lpmf<true, double, double, double>(y, eta,
+                                                                         phi)),
+      (stan::math::neg_binomial_2_log_log<true, double, double, double>(y, eta,
+                                                                        phi)));
   EXPECT_FLOAT_EQ(
-      (stan::math::neg_binomial_2_lpmf<false, double, double, double>(y, mu,
-                                                                      phi)),
-      (stan::math::neg_binomial_2_log<false, double, double, double>(y, mu,
-                                                                     phi)));
-  EXPECT_FLOAT_EQ(
-      (stan::math::neg_binomial_2_lpmf<double, double, double>(y, mu, phi)),
-      (stan::math::neg_binomial_2_log<double, double, double>(y, mu, phi)));
+      (stan::math::neg_binomial_2_log_lpmf<false, double, double, double>(
+          y, eta, phi)),
+      (stan::math::neg_binomial_2_log_log<false, double, double, double>(y, eta,
+                                                                         phi)));
+  EXPECT_FLOAT_EQ((stan::math::neg_binomial_2_log_lpmf<double, double, double>(
+                      y, eta, phi)),
+                  (stan::math::neg_binomial_2_log_log<double, double, double>(
+                      y, eta, phi)));
+}
+
+TEST(ProbDistributionsNegBinomial2Log, 
+  neg_binomial_2_log_grid_test) {
+  std::vector<double> mu_log_to_test = 
+    {-101,-27, -3, -1, -0.132, 0, 4, 10, 87 };
+  std::vector<double> phi_to_test = {2e-5,0.36,1, 2.3e5, 1.8e10, 6e16  };
+  std::vector<int> n_to_test = {0, 1, 10, 39, 101, 3048, 150054 };
+
+  auto tolerance = [](double x) { return std::max(fabs(x * 1e-8), 1e-14); };
+
+  for(double mu_log : mu_log_to_test) { 
+    for(double phi : phi_to_test) {
+      for(int n : n_to_test) {
+          double val_log = stan::math::neg_binomial_2_log_lpmf(n, mu_log, phi);
+          EXPECT_LE(val_log, 0) << "neg_binomial_2_log_lpmf yields " << 
+            val_log << " which si greater than 0 for n = " << n << 
+            ", mu_log = " << mu_log << ", phi = " << phi << ".";
+          double val_orig = 
+            stan::math::neg_binomial_2_lpmf(n, std::exp(mu_log), phi);
+          EXPECT_NEAR(val_log, val_orig, tolerance(val_orig)) <<
+            "neg_binomial_2_log_lpmf yields different result (" << val_log << 
+            ") than neg_binomial_2_lpmf (" << val_orig << ") for n = " << n << 
+            ", mu_log = " << mu_log << ", phi = " << phi << ".";
+      }
+    }
+  }
+  
 }
